2d6ae29dce70edddcb60c8d3f9c600533e36b395
Dima Rudenko <dimchik.ru@gmail.com>
2013-04-01 00:22:21 +0300

    add full sprite

--- js/level.js
+++ js/level.js
@@ -10,14 +10,15 @@ function gameStart(scene) {
     stats.begin();
 
     var context = scene.context;
-    context.beginPath();
-    context.rect(0, 0, this.width, this.height);
-    context.closePath();
-    context.fill();
-    context.font = '10pt Arial';
-    context.strokeStyle = 'white';
-    context.strokeText('Level:  ' + level, 20, 20);
-    context.strokeText('Score:  ' + score, scene.getWidth()-80, 20);
+
+    context.fillStyle = "rgba(0, 0, 0, 1)";
+    context.fillRect(0, 0, this.width, this.height);
+
+
+    context.font = 'bold 10pt Arial';
+    context.fillStyle = "rgba(211, 211, 211, 1)";
+    context.fillText('Level:  ' + level, 20, 20);
+    context.fillText('Score:  ' + score, scene.getWidth()-80, 20);
 
     if (aliens.length === 0) {
         levelUp();
@@ -31,15 +32,17 @@ function gameStart(scene) {
 function gameOver() {
     scene.gameLoop(function (scene) {
         var context = scene.context;
-        context.beginPath();
+
+        context.fillStyle = "rgba(0, 0, 0, 1)";
         context.fillRect(0, 0, this.width, this.height);
-        context.closePath();
+
         context.strokeStyle = 'white';
         context.textAlign = 'center';
         context.font = "bold 30pt Arial";
         context.strokeText("Game Over", scene.getWidth() / 2, scene.getHeight() / 2);
+        context.strokeText("You score: "+ score, scene.getWidth() / 2, scene.getHeight() / 2 +40);
         context.font = '10pt Arial';
-        context.strokeText('press start  ', scene.getWidth() / 2, scene.getHeight() / 2 + 50);
+        context.strokeText('press start  ', scene.getWidth() / 2, scene.getHeight() / 2 + 60);
 
     });
 }
