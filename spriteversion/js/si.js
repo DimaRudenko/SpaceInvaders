ae126dd3ba793553f10f4c8fc1db21590cc6e58b
Dima Rudenko <dimchik.ru@gmail.com>
2013-04-02 12:07:26 +0300

    style

--- spriteversion/js/si.js
+++ spriteversion/js/si.js
@@ -47,34 +47,36 @@ SI.detectColision = function (firsObj, secondObj) {
  * Provides requestAnimationFrame in a cross browser way.
  * @author paulirish  http://paulirish.com/
  */
+(function(){
 
-if (!window.requestAnimationFrame) {
+    if (!window.requestAnimationFrame) {
 
-    window.requestAnimationFrame = (function () {
+        window.requestAnimationFrame = (function () {
 
-        return window.webkitRequestAnimationFrame ||
-            window.mozRequestAnimationFrame ||
-            window.oRequestAnimationFrame ||
-            window.msRequestAnimationFrame ||
-            function (/* function FrameRequestCallback */ callback, /* DOMElement Element */ element) {
+            return window.webkitRequestAnimationFrame ||
+                window.mozRequestAnimationFrame ||
+                window.oRequestAnimationFrame ||
+                window.msRequestAnimationFrame ||
+                function (/* function FrameRequestCallback */ callback, /* DOMElement Element */ element) {
 
-                window.setTimeout(callback, 1000 / 60);
+                    window.setTimeout(callback, 1000 / 60);
 
-            };
+                };
 
-    })();
-}
+        })();
+    }
 
-if (!window.cancelRequestAnimFrame) {
+    if (!window.cancelRequestAnimFrame) {
 
-    window.cancelRequestAnimFrame = (function () {
+        window.cancelRequestAnimFrame = (function () {
 
-        return window.cancelAnimationFrame ||
-            window.webkitCancelRequestAnimationFrame ||
-            window.mozCancelRequestAnimationFrame ||
-            window.oCancelRequestAnimationFrame ||
-            window.msCancelRequestAnimationFrame ||
-            clearTimeout;
-    })();
-}
+            return window.cancelAnimationFrame ||
+                window.webkitCancelRequestAnimationFrame ||
+                window.mozCancelRequestAnimationFrame ||
+                window.oCancelRequestAnimationFrame ||
+                window.msCancelRequestAnimationFrame ||
+                clearTimeout;
+        })();
+    }
 
+}())
